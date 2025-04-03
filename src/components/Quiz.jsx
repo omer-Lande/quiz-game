d37ccfd8f69cbc5e0b2dbf4f18ee2982aab03964
Omer Lande <omerl@log-on.com>
2025-04-03 12:39:03 +0300

    fixed proggres bar for every question

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -40,7 +40,11 @@ export default function Quiz() {
   return (
     <div id="quiz">
       <div id="question">
-        <QuestionTimer timeout={10000} onTimeout={hadnleSkipAnswer} />
+        <QuestionTimer
+          key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={hadnleSkipAnswer}
+        />
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
