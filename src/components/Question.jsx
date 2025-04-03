6cf848635d2ea8a0298603df09bac2335bb04d68
Omer Lande <omerl@log-on.com>
2025-04-03 14:55:46 +0300

    fixed timer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -9,22 +9,32 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
     isCorrect: null,
   });
 
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
+
   function handleSelectAnswer(answer) {
     setAnswer({
       selectedAnswer: answer,
       isCorrect: null,
     });
-  }
-  setTimeout(() => {
-    setAnswer({
-      selectedAnswer: answer,
-      isCorrect: QUESTIONS[index].answers[0] === answer,
-    });
-
     setTimeout(() => {
-      onSelectAnswer(answer);
-    }, 2000);
-  }, 1000);
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: QUESTIONS[index].answers[0] === answer,
+      });
+
+      setTimeout(() => {
+        onSelectAnswer(answer);
+      }, 2000);
+    }, 1000);
+  }
 
   let answerState = "";
 
@@ -36,7 +46,12 @@ export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
 
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === "" ? onSkipAnswer : null}
+        mode={answerState}
+      />
       <h2>{QUESTIONS[index].text}</h2>
       <Answers
         answers={QUESTIONS[index].answers}
